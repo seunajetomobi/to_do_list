a8918ee1fb1e8ba149be1da99e72621717e57d3f
R2D2 JINJA <r2d2@localhost>
2026-03-25 11:38:19 +0100

    chore: bump build marker to force new hashed assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// include a tiny build timestamp to force a new bundle hash on rebuild
+// (helps bust stubborn caches when redeploying to static hosts)
+const __BUILD_TS__ = "20260325-" + String(Date.now());
+console.log("main: build", __BUILD_TS__);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
